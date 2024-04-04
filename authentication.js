d51a89a4d86782382631bb80ed970c184661ca8d
wolfiee42 <saifalislam2022@gmail.com>
2024-04-04 10:09:31 +0600

    login page created

--- authentication.js
+++ authentication.js
@@ -44,3 +44,28 @@ const getValue = (id) => {
     const value = document.getElementById(id).value;
     return value;
 };
+
+
+const handleLogin = (event) => {
+    event.preventDefault();
+    const username = getValue("login-username");
+    const password = getValue("login-password");
+    console.log(username, password);
+    if ((username, password)) {
+        fetch("https://wellness-oasis-clinic-api.onrender.com/patients/login/", {
+            method: "POST",
+            headers: { "content-type": "application/json" },
+            body: JSON.stringify({ username, password }),
+        })
+            .then((res) => res.json())
+            .then((data) => {
+                console.log(data);
+
+                if (data.token && data.user_id) {
+                    localStorage.setItem("token", data.token);
+                    localStorage.setItem("user_id", data.user_id);
+                    window.location.href = "index.html";
+                }
+            });
+    }
+};
